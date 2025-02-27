f57f7764d42ef152e9be83139275c6b87f0f6bc6
li003454 <153538215+li003454@users.noreply.github.com>
2025-02-27 13:47:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from age import Age
 import os
 
 api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_API_KEY")
 age = Age(openai_api_key=api_key)
-query = input("Enter your query: ")
-age.run_query(query)
+age.run_query()
